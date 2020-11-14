ca23761eb840f951d423ef7f385648a8b289e81b
kazu <2020kazutaka@gmail.com>
2020-11-15 02:30:18 +0900

    Add: TaskモデルのRspecを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title) { |n| "test#{n}" }
+    content { "テスト用のタスクです"}
+    status { 0 }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:task) { FactoryBot.create(:task) }
+    let(:another_task) { FactoryBot.create(:task) }
+
+    it 'is valid with all attributes' do
+      expect(task).to be_valid
+    end
+
+    it 'is invalid without title' do
+      task.title = ''
+      expect(task).to be_invalid
+    end
+
+    it 'is invalid without status' do
+      task.status = ''
+      expect(task).to be_invalid
+    end
+
+    it 'is invalid with a duplicate title' do
+      another_task = task.dup
+      expect(another_task).to be_invalid
+    end
+
+    it 'is valid with a another title' do
+      expect(another_task).to be_valid
+    end
+  end
 end
