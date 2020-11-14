83e6da83f8aef0f41e5a456d5e519f3e7e39f5be
kazu <2020kazutaka@gmail.com>
2020-11-15 03:10:16 +0900

    Fix: Factoryのuserモデルを修正
    emailカラムにsequenceを使用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "example@example.com" }
+    sequence(:email) { |n| "example#{n}@example.com" }
     password { "foobar" }
     password_confirmation { "foobar" }
   end
